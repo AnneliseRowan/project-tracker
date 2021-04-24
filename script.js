d68e140325c5f37bdc2933fc08a368c024daf5c2
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-04-24 13:58:04 -0400

    added more code and some design

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+var todayDate = setInterval(function(){ 
+    var today = moment();
+    $("#time").text(today.format('MMMM Do YYYY, h:mm:ss a')); 
+}, 1000);
+
+$( function() {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: false,
+    });
+});
+
+
